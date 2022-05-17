1a7d8d61899ebfcfc8e48991d3e2507bd2c33944
Blaze Scott <blazeascott@gmail.com>
2022-05-17 01:11:45 -0500

    Refactoring for cleaner code
    I replaced a lot of html for symanticly correct html... At least I hope it's
    symanticly correct.
    
    Shortened a lot of names in the app.js file, and condensed several variables
    into a single array since them being called indavidualy wasn't necisary.

--- app.js
+++ app.js
@@ -1,32 +1,19 @@
 const convertBtn = document.getElementById('convert-btn');
 
-const metersIntEl = document.getElementById('meters-int-el');
-const metersFloatEl = document.getElementById('meters-float-el');
-const feetIntEl = document.getElementById('feet-int-el');
-const feetFloatEl = document.getElementById('feet-float-el');
-const litersIntEl = document.getElementById('liters-int-el');
-const litersFloatEl = document.getElementById('liters-float-el');
-const gallonsIntEl = document.getElementById('gallons-int-el');
-const gallonsFloatEl = document.getElementById('gallons-float-el');
-const kilosIntEl = document.getElementById('kilos-int-el');
-const kilosFloatEl = document.getElementById('kilos-float-el');
-const poundsIntEl = document.getElementById('pounds-int-el');
-const poundsFloatEl = document.getElementById('pounds-float-el');
+const metersEl = document.getElementById('meters-float-el');
+const feetEl = document.getElementById('feet-float-el');
+const litersEl = document.getElementById('liters-float-el');
+const gallonsEl = document.getElementById('gallons-float-el');
+const kilosEl = document.getElementById('kilos-float-el');
+const poundsEl = document.getElementById('pounds-float-el');
+const allFloatEls = [metersEl, feetEl, litersEl, gallonsEl, kilosEl, poundsEl];
 const allIntEls = [
-    metersIntEl,
-    feetIntEl,
-    litersIntEl,
-    gallonsIntEl,
-    kilosIntEl,
-    poundsIntEl,
-];
-const allFloatEls = [
-    metersFloatEl,
-    feetFloatEl,
-    litersFloatEl,
-    gallonsFloatEl,
-    kilosFloatEl,
-    poundsFloatEl,
+    document.getElementById('meters-int-el'),
+    document.getElementById('feet-int-el'),
+    document.getElementById('liters-int-el'),
+    document.getElementById('gallons-int-el'),
+    document.getElementById('kilos-int-el'),
+    document.getElementById('pounds-int-el'),
 ];
 
 const roundThree = (num) => Number.parseFloat(num).toFixed(3);
@@ -35,8 +22,8 @@ const convertLength = (inputNum) => {
     // 1 meter * 3.28084 feet
     const feetRatio = 3.28084;
     return {
-        feetFloat: roundThree(inputNum * feetRatio),
-        metersFloat: roundThree(inputNum / feetRatio),
+        feet: roundThree(inputNum * feetRatio),
+        meters: roundThree(inputNum / feetRatio),
     };
 };
 
@@ -44,8 +31,8 @@ const convertVolume = (inputNum) => {
     // 1 liter / 3.785 gallons
     const gallonsRatio = 3.785;
     return {
-        litersFloat: roundThree(inputNum / gallonsRatio),
-        gallonsFloat: roundThree(inputNum * gallonsRatio),
+        liters: roundThree(inputNum / gallonsRatio),
+        gallons: roundThree(inputNum * gallonsRatio),
     };
 };
 
@@ -53,8 +40,8 @@ const convertMass = (inputNum) => {
     // 1 kilo * 2.2 pounds
     const poundsRatio = 2.2;
     return {
-        kilosFloat: roundThree(inputNum * poundsRatio),
-        poundsFloat: roundThree(inputNum / poundsRatio),
+        kilos: roundThree(inputNum * poundsRatio),
+        pounds: roundThree(inputNum / poundsRatio),
     };
 };
 
@@ -71,12 +58,13 @@ const setValues = (inputNum) => {
 
     allIntEls.map((el) => (el.innerText = inputNum));
 
-    metersFloatEl.innerText = length.metersFloat;
-    feetFloatEl.innerText = length.feetFloat;
-    litersFloatEl.innerText = volume.litersFloat;
-    gallonsFloatEl.innerText = volume.gallonsFloat;
-    kilosFloatEl.innerText = mass.kilosFloat;
-    poundsFloatEl.innerText = mass.poundsFloat;
+    metersEl.innerText = length.meters;
+    feetEl.innerText = length.feet;
+    litersEl.innerText = volume.liters;
+    gallonsEl.innerText = volume.gallons;
+    kilosEl.innerText = mass.kilos;
+    poundsEl.innerText = mass.pounds;
+    return;
 };
 
 convertBtn.addEventListener('click', () => {
