9a096b54128cbf647b03c9d2c83b6a5b4abf8c9e
Blaze Scott <blazeascott@gmail.com>
2022-05-18 05:18:35 -0500

    Implimented a rework on the css
    Mostly css work here. Got all the styling set up, and it looks pretty good.

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const convertBtn = document.getElementById('convert-btn');
 
+// Adjusted the names to be clearer
 const metersEl = document.getElementById('meters-float-el');
 const feetEl = document.getElementById('feet-float-el');
 const litersEl = document.getElementById('liters-float-el');
@@ -7,6 +8,8 @@ const gallonsEl = document.getElementById('gallons-float-el');
 const kilosEl = document.getElementById('kilos-float-el');
 const poundsEl = document.getElementById('pounds-float-el');
 const allFloatEls = [metersEl, feetEl, litersEl, gallonsEl, kilosEl, poundsEl];
+
+// Removed the variables for the int numbers, and put the dom getters (not sure what to call them) into an array
 const allIntEls = [
     document.getElementById('meters-int-el'),
     document.getElementById('feet-int-el'),
@@ -16,14 +19,15 @@ const allIntEls = [
     document.getElementById('pounds-int-el'),
 ];
 
-const roundThree = (num) => Number.parseFloat(num).toFixed(3);
+// Renamed to something that made more sense
+const set3Decimal = (num) => Number.parseFloat(num).toFixed(3);
 
 const convertLength = (inputNum) => {
     // 1 meter * 3.28084 feet
     const feetRatio = 3.28084;
     return {
-        feet: roundThree(inputNum * feetRatio),
-        meters: roundThree(inputNum / feetRatio),
+        feet: set3Decimal(inputNum * feetRatio),
+        meters: set3Decimal(inputNum / feetRatio),
     };
 };
 
@@ -31,8 +35,8 @@ const convertVolume = (inputNum) => {
     // 1 liter / 3.785 gallons
     const gallonsRatio = 3.785;
     return {
-        liters: roundThree(inputNum / gallonsRatio),
-        gallons: roundThree(inputNum * gallonsRatio),
+        liters: set3Decimal(inputNum / gallonsRatio),
+        gallons: set3Decimal(inputNum * gallonsRatio),
     };
 };
 
@@ -40,24 +44,27 @@ const convertMass = (inputNum) => {
     // 1 kilo * 2.2 pounds
     const poundsRatio = 2.2;
     return {
-        kilos: roundThree(inputNum * poundsRatio),
-        pounds: roundThree(inputNum / poundsRatio),
+        kilos: set3Decimal(inputNum * poundsRatio),
+        pounds: set3Decimal(inputNum / poundsRatio),
     };
 };
 
 const setValues = (inputNum) => {
-    // check for zero
+    // check for zero, and set all numbers to zero
     if (inputNum === 0) {
         allIntEls.map((el) => (el.innerText = '0'));
         allFloatEls.map((el) => (el.innerText = '0.000'));
         return;
     }
+
     const length = convertLength(inputNum);
     const volume = convertVolume(inputNum);
     const mass = convertMass(inputNum);
 
+    // Set all the int numbers on the page
     allIntEls.map((el) => (el.innerText = inputNum));
 
+    // Set all the float numbers on the page
     metersEl.innerText = length.meters;
     feetEl.innerText = length.feet;
     litersEl.innerText = volume.liters;
